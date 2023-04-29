0e8924613ef83f9b2eac52e926fb0af231bfcf1d
Katerina Shibalovich <katerinaharak77775555@gmail.com>
2023-04-29 14:08:12 +0300

    fix: corrected task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
 const path = require('path');
 let readStream = fs.createReadStream(path.join(__dirname,"text.txt"), {encoding: 'utf-8'});
-let data = '';
-readStream.on('data', function(chunk) {
-    data += chunk;
-})
+readStream.on("data", (data) => console.log(data));
